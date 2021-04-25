afab433a32c521c286e4aa6f240b9eeb16114910
dohuuhuy <dohuuhuy2@gmail.com>
2021-04-25 23:04:38 +0700

    okey

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbreact/dist/css/mdb.css'
-import React from 'react'
 import { DefaultSeo } from 'next-seo'
-import SEO from '../next-seo.config'
+import React from 'react'
+import SEO from './../next-seo.config'
 
 const MyApp = ({ Component, pageProps }: any) => {
   const LayoutWrapper = Component.Layout ? Component.Layout : React.Fragment
   return (
-    <DefaultSeo {...SEO}>
-      <LayoutWrapper>
+    <LayoutWrapper>
+      <DefaultSeo {...SEO}>
         <Component {...pageProps} />
-      </LayoutWrapper>
-    </DefaultSeo>
+      </DefaultSeo>
+    </LayoutWrapper>
   )
 }
 
